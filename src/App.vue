c6f227b594f64cbc123dbff6f7adb9e4ee21bfa8
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-12-12 16:00:03 -0500

    App has Table component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <Table />
   </div>
 </template>
 
 <script>
+import Table from './components/Table.vue'
+
 export default {
   name: 'app',
   components: {
+    Table
   }
 }
 </script>
