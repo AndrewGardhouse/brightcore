30b16684997751ae93018538f33834a9407f619f
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-12-12 11:38:31 -0500

    Adds sortByColumn mutation for sorting columns

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -2,5 +2,14 @@ export default {
   updateTableRow(state, payload) {
     const row = state.tableData.find(row => row['ID'] === payload['ID'])
     Object.assign(row, payload);
+  },
+  sortByColumn(state, payload) {
+    state.tableData = state.tableData.sort((a, b) => {
+      if (payload.direction == 'desc') {
+        return a[payload.column] < b[payload.column]
+      } else if (payload.direction == 'asc') {
+        return a[payload.column] > b[payload.column]
+      }
+    })
   }
 }
--- tests/unit/store/mutations.spec.js
+++ tests/unit/store/mutations.spec.js
@@ -1,20 +1,54 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import state from '@/store/state'
 import mutations from '@/store/mutations'
 
 Vue.use(Vuex)
 
 describe('mutations', () => {
-  it('updateTableRow', () => {
-    const store = new Vuex.Store({
-      state,
+  let store
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      state: {
+        tableData: [
+          {
+            'ID': '3471DA17-401F-9633-BF81-4CADA6FD5C79',
+            'Name': 'Kyra Lester',
+            'Description': 'Curabitur dictum. Phasellus in',
+            'Date': '2017-07-23T04:24:49-07:00',
+            'Amount': 34554
+          },
+          {
+            'ID': '9F5C9912-936A-FB85-1EDB-9DA87BE7FF1E',
+            'Name': 'Buckminster Alvarado',
+            'Description': 'dui, in sodales elit erat vitae risus. Duis a mi',
+            'Date': '2018-11-08T05:44:15-08:00',
+            'Amount': 67708
+          },
+          {
+            'ID': 'B743AC82-3613-13A2-2E42-E0C1F5CBF8A6',
+            'Name': 'Athena Smith',
+            'Description': 'massa lobortis ultrices. Vivamus rhoncus.',
+            'Date': '2018-11-11T06:19:57-08:00',
+            'Amount': 7367
+          },
+          {
+            'ID': '74749D4F-A43F-34E8-A687-D54924B17251',
+            'Name': 'Cameron Thompson',
+            'Description': 'dolor. Fusce mi lorem, vehicula et, rutrum eu,',
+            'Date': '2019-09-30T06:56:15-07:00',
+            'Amount': 80760
+          }
+        ]
+      },
       mutations
     })
+  })
 
+  it('updateTableRow', () => {
     const ID = '3471DA17-401F-9633-BF81-4CADA6FD5C79'
     const Description = 'new description'
-    const updatedRow = state.tableData.find(row => row['ID'] === ID)
+    const updatedRow = store.state.tableData.find(row => row['ID'] === ID)
 
     expect(updatedRow['Description']).not.toEqual(Description)
 
@@ -24,5 +58,49 @@ describe('mutations', () => {
     })
 
     expect(updatedRow['Description']).toEqual(Description)
+  })
+
+  it('sortByColumn with Amount', () => {
+    store.commit('sortByColumn', {
+      column: 'Amount',
+      direction: 'desc'
+    })
+
+    expect(store.state.tableData[0]['Amount']).toEqual(80760)
+    expect(store.state.tableData[1]['Amount']).toEqual(67708)
+    expect(store.state.tableData[2]['Amount']).toEqual(34554)
+    expect(store.state.tableData[3]['Amount']).toEqual(7367)
+
+    store.commit('sortByColumn', {
+      column: 'Amount',
+      direction: 'asc'
+    })
+
+    expect(store.state.tableData[0]['Amount']).toEqual(7367)
+    expect(store.state.tableData[1]['Amount']).toEqual(34554)
+    expect(store.state.tableData[2]['Amount']).toEqual(67708)
+    expect(store.state.tableData[3]['Amount']).toEqual(80760)
+  })
+
+  it('sortByColumn with ID', () => {
+    store.commit('sortByColumn', {
+      column: 'ID',
+      direction: 'desc'
+    })
+
+    expect(store.state.tableData[0]['ID']).toEqual('B743AC82-3613-13A2-2E42-E0C1F5CBF8A6')
+    expect(store.state.tableData[1]['ID']).toEqual('9F5C9912-936A-FB85-1EDB-9DA87BE7FF1E')
+    expect(store.state.tableData[2]['ID']).toEqual('74749D4F-A43F-34E8-A687-D54924B17251')
+    expect(store.state.tableData[3]['ID']).toEqual('3471DA17-401F-9633-BF81-4CADA6FD5C79')
+
+    store.commit('sortByColumn', {
+      column: 'ID',
+      direction: 'asc'
+    })
+
+    expect(store.state.tableData[0]['ID']).toEqual('3471DA17-401F-9633-BF81-4CADA6FD5C79')
+    expect(store.state.tableData[1]['ID']).toEqual('74749D4F-A43F-34E8-A687-D54924B17251')
+    expect(store.state.tableData[2]['ID']).toEqual('9F5C9912-936A-FB85-1EDB-9DA87BE7FF1E')
+    expect(store.state.tableData[3]['ID']).toEqual('B743AC82-3613-13A2-2E42-E0C1F5CBF8A6')
   })
 })
