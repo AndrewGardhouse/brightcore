09ef59e9cdb0b9aaf1e70548c32184ff6f1715a0
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-12-17 23:31:05 -0500

    Table shows '0 matches' if there are no rows to display

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="table-wrapper">
     <TableHeaders />
-    <TableRow v-for="row in filteredSortedTableData"
-              :key="row['ID']"
-              :id="row['ID']"
-              :name="row['Name']"
-              :description="row['Description']"
-              :date="row['Date']"
-              :amount="row['Amount']" />
+    <div v-if="filteredSortedTableData.length > 0">
+      <TableRow v-for="row in filteredSortedTableData"
+                :key="row['ID']"
+                :id="row['ID']"
+                :name="row['Name']"
+                :description="row['Description']"
+                :date="row['Date']"
+                :amount="row['Amount']" />
+    </div>
+    <div v-else>
+      <h2 class="no-rows">0 matches</h2>
+    </div>
   </div>
 </template>
 
@@ -34,6 +39,9 @@ export default {
   border: 1px solid #2c3e50;
   .table-row:nth-child(even) {
     background-color: #e6eeef;
+  }
+  .no-rows {
+    text-align: center;
   }
 }
 </style>
