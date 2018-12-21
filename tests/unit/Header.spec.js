cd4edd439d0b33075214973fe7c1fe4db36dfae6
Andrew Gardhouse <andrewgardhouse@macbook-air.gateway.2wire.net>
2018-12-21 13:39:47 -0500

    Adds test for setting Header sortDirectionIndex if sortBy and sortDirection is set when component is mounted

--- tests/unit/Header.spec.js
+++ tests/unit/Header.spec.js
@@ -105,3 +105,67 @@ describe('Header.vue', () => {
     expect(descArrow.classes()).not.toContain('active')
   })
 })
+
+describe('Header mounted()', () => {
+  it('should set the correct sortDirectionIndex if sortDirection and sortBy exist in state', () => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+
+    const store = new Vuex.Store({
+      state: {
+        sortBy: 'Description',
+        sortDirection: 'asc',
+        tableData: [
+          {
+            'ID': '3471DA17-401F-9633-BF81-4CADA6FD5C79',
+            'Name': 'Kyra Lester',
+            'Description': 'Curabitur dictum. Phasellus in',
+            'Date': '2017-07-23T04:24:49-07:00',
+            'Amount': 34554
+          },
+          {
+            'ID': '9F5C9912-936A-FB85-1EDB-9DA87BE7FF1E',
+            'Name': 'Buckminster Alvarado',
+            'Description': 'dui, in sodales elit erat vitae risus. Duis a mi',
+            'Date': '2018-11-08T05:44:15-08:00',
+            'Amount': 67708
+          },
+          {
+            'ID': 'B743AC82-3613-13A2-2E42-E0C1F5CBF8A6',
+            'Name': 'Athena Smith',
+            'Description': 'massa lobortis ultrices. Vivamus rhoncus.',
+            'Date': '2018-11-11T06:19:57-08:00',
+            'Amount': 7367
+          },
+          {
+            'ID': '74749D4F-A43F-34E8-A687-D54924B17251',
+            'Name': 'Cameron Thompson',
+            'Description': 'dolor. Fusce mi lorem, vehicula et, rutrum eu,',
+            'Date': '2019-09-30T06:56:15-07:00',
+            'Amount': 80760
+          }
+        ]
+      },
+      mutations
+    })
+
+    const descriptionWrapper = shallowMount(Header, {
+      propsData: {
+        header: 'Description'
+      },
+      store,
+      localVue
+    })
+
+    const nameWrapper = shallowMount(Header, {
+      propsData: {
+        header: 'Name'
+      },
+      store,
+      localVue
+    })
+
+    expect(descriptionWrapper.vm.sortDirectionIndex).toBe(1)
+    expect(nameWrapper.vm.sortDirectionIndex).toBe(0)
+  })
+})
